204981991d57511fb55f53b3f7d5ef2bc7f898d5
Utkarsh Sharma <flamingice2801@gmail.com>
2019-10-14 19:59:35 +0530

    Update countDigits.java

--- LEVEL_00/countDigits.java
+++ LEVEL_00/countDigits.java
@@ -1,11 +1,10 @@
-public class countDigits 
-{
-	public static void main(String[] args) 
-  {
+import java.util.Scanner;
+
+public class countDigits {
+	public static void main(String[] args) {
 		int num = 98123;
-		
-		while(num != 0)
-    {
+
+		while (num != 0) {
 			int remainder = num % 10;
 			System.out.print(remainder);
 			num = num / 10;

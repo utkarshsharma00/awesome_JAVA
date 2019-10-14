6289e21a17820eb29700da49af2de9fffe7f54d9
Utkarsh Sharma <flamingice2801@gmail.com>
2019-10-14 20:00:35 +0530

    Update SimpleInterest.java

--- LEVEL_00/SimpleInterest.java
+++ LEVEL_00/SimpleInterest.java
@@ -1,13 +1,11 @@
 import java.util.Scanner;
 
-public class SimpleInterest 
-{
-	public static void main(String[] args) 
-  {
+public class SimpleInterest {
+	public static void main(String[] args) {
 		int principal = 1000;
 		int rate = 10;
 		int time = 2;
-		int simple_interest = principal * rate * time /100;
+		int simple_interest = principal * rate * time / 100;
 		System.out.println("Simple Interes is " + simple_interest);
 	}
 }

0928ae3c3805cfb4283babc042af66717031f4a6
Utkarsh Sharma <flamingice2801@gmail.com>
2019-10-14 20:00:11 +0530

    Update HelloWorld.java

--- LEVEL_00/HelloWorld.java
+++ LEVEL_00/HelloWorld.java
@@ -1,10 +1,7 @@
 import java.util.*
 
-public class HelloWorld 
-{
-
-	public static void main(String[] args) 
-	{
+public class HelloWorld {
+	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
 

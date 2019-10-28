ab7e0a832cb301d8a2b8fa9f82811bb3e49b74cd
Utkarsh Sharma <flamingice2801@gmail.com>
2019-10-28 17:12:44 +0530

    Create pat6.java

---
+++ LEVEL_02/pat6.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class pat6 {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+
+		System.out.println("Please enter a number ");
+		int n = scn.nextInt();
+
+		int stars = 1;
+		int spaces = n / 2;
+		//yet to be written
+	}
+
+}
+
+
+
+
+
+
+
+
+
+

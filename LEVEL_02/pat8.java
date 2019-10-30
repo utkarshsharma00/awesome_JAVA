54814525447a430491aac8db922b1e58ff00430f
Utkarsh Sharma <flamingice2801@gmail.com>
2019-10-30 21:39:31 +0530

    Create pat8.java

---
+++ LEVEL_02/pat8.java
@@ -0,0 +1,18 @@
+package feb3;
+
+import java.util.Scanner;
+
+public class pat8 {
+
+	public static void main(String[] args) {
+		int[] arr = { 2, 7, 3, 4, 5 };
+		int max = arr[0];
+		for (int i = 1; i <= 4; i++) {
+			if (arr[i] > max) {
+				max = arr[i];
+			}
+		}
+  //TODO
+	}
+
+}

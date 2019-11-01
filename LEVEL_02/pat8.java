16a79990f7b39964c94bdfe2dc39f4cc084b457c
Utkarsh Sharma <flamingice2801@gmail.com>
2019-11-01 20:27:22 +0530

    Update pat8.java

--- LEVEL_02/pat8.java
+++ LEVEL_02/pat8.java
@@ -1,8 +1,6 @@
-package feb3;
-
 import java.util.Scanner;
 
-public class pat8 {
+public class pat9 {
 
 	public static void main(String[] args) {
 		int[] arr = { 2, 7, 3, 4, 5 };
@@ -12,7 +10,17 @@ public class pat8 {
 				max = arr[i];
 			}
 		}
-  //TODO
+
+		for (int i = 1; i <= max; i++) {
+			for (int j = 0; j <= 4; j++) {
+				if (i >= max - arr[j] + 1) {
+					System.out.print("*");
+				} else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
 	}
 
 }

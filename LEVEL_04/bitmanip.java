73e2185962208a654537f12a6e4e670841239bb5
Utkarsh Sharma <flamingice2801@gmail.com>
2019-11-04 19:55:22 +0530

    Create bitmanip.java

---
+++ LEVEL_04/bitmanip.java
@@ -0,0 +1,21 @@
+public class bitmanip {
+
+	public static void main(String[] args) {
+		int num = 57;
+		System.out.println(num + " " + Integer.toBinaryString(num));
+
+		int k = 3;
+		// bit on logic
+		int bm = 1 << k;
+		System.out.println(bm + " " + Integer.toBinaryString(bm));
+		
+		if((num & bm) == 0){
+			System.out.println("Bit is off");
+			num = (num | bm);
+			System.out.println(num + " " + Integer.toBinaryString(num));
+		} else {
+			System.out.println("Bit is already on.");
+		}
+	}
+
+}

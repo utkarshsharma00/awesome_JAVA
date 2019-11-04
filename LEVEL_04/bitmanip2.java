ad58ff7db1fe8c26864eb74e02fc339c08c33034
Utkarsh Sharma <flamingice2801@gmail.com>
2019-11-04 19:56:46 +0530

    Create bitmanip2.java

---
+++ LEVEL_04/bitmanip2.java
@@ -0,0 +1,10 @@
+public class bitmanip2 {
+
+	public static void main(String[] args) {
+		int num = 57;
+		System.out.println(num + " " + Integer.toBinaryString(num));
+
+		int k = 3;
+		//TODO
+
+}

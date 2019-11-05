52c87b1bcc8b4dd51025c8959b1e373bc4d902c2
Utkarsh Sharma <flamingice2801@gmail.com>
2019-11-05 20:12:04 +0530

    Create binarysearch.java

---
+++ LEVEL_05/binarysearch.java
@@ -0,0 +1,15 @@
+import java.util.*
+
+public class binarySearch {
+
+	public static void main(String[] args) {
+		int[] arr = {10, 10, 10, 20, 20, 20, 20, 20, 20, 30, 30, 30};
+		int data = 10;
+		
+		int lo = 0;
+		int hi = arr.length - 1;
+		int res = -1;
+    
+    }
+
+}    

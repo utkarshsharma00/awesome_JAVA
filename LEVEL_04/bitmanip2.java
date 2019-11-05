339457efca3fb96dc455567d86ede6a47c8a032c
Utkarsh Sharma <flamingice2801@gmail.com>
2019-11-05 20:09:43 +0530

    Update bitmanip2.java

--- LEVEL_04/bitmanip2.java
+++ LEVEL_04/bitmanip2.java
@@ -7,6 +7,18 @@ public class bitmanip2 {
 		System.out.println(num + " " + Integer.toBinaryString(num));
 
 		int k = 3;
-		//TODO
+		// bit on logic
+		int bm = 1 << k;
+		System.out.println(bm + " " + Integer.toBinaryString(bm));
+		
+		if((num & bm) == 0){
+			System.out.println("Bit is already off");
+			System.out.println(num + " " + Integer.toBinaryString(num));
+		} else {
+			System.out.println("Bit is on.");
+			num = (num & ~bm);
+			System.out.println(num + " " + Integer.toBinaryString(num));
+		}
+	}
 
 }

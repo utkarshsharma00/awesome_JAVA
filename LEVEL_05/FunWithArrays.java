1d6932bb540a78caf0f306370f95b78f8399c71f
Utkarsh Sharma <flamingice2801@gmail.com>
2019-11-06 21:00:20 +0530

    Create FunWithArrays.java

---
+++ LEVEL_05/FunWithArrays.java
@@ -0,0 +1,15 @@
+public class FunWithArrays {
+
+	public static void main(String[] args) {
+//		int[] arr = new int[4];
+//		arr[0] = 10;
+//		arr[1] = 20;
+//		arr[2] = 30;
+//		arr[3] = 40;
+		int[] arr = {10, 20, 30, 40};
+		for(int i = 0; i < arr.length; i++){
+			System.out.println(arr[i]);
+		}
+	}
+
+}

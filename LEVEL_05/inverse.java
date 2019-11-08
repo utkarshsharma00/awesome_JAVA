c4d590481ade5b347eeb43340f3118ff5a651fb3
Utkarsh Sharma <flamingice2801@gmail.com>
2019-11-08 17:55:28 +0530

    Create inverse.java

---
+++ LEVEL_05/inverse.java
@@ -0,0 +1,24 @@
+import java.util.*
+
+public class inverse {
+
+	public static void main(String[] args) {
+		int[] arr = {4, 1, 2, 0, 3};
+		int[] inv = new int[arr.length];
+		
+		for(int i = 0; i < arr.length; i++){
+			inv[arr[i]] = i;
+		}
+		
+		for(int i = 0; i < inv.length; i++){
+			System.out.print(inv[i] + " ");
+		}
+	}
+
+}
+
+
+
+
+
+

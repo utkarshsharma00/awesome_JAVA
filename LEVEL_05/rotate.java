df865dec74eaf7dab8f8b1ccdf9b35a29a093294
Utkarsh Sharma <flamingice2801@gmail.com>
2019-11-09 19:50:42 +0530

    Create rotate.java

---
+++ LEVEL_05/rotate.java
@@ -0,0 +1,21 @@
+import java.util.*
+
+public class rotate {
+
+	public static void main(String[] args) {
+		int[] arr = {2, 3, 5, 7, 1, 6, 9, 0};
+		int r = 2;
+		
+		reverse(arr, 0, arr.length - 1); // full
+		reverse(arr, 0, r - 1); // p1
+		reverse(arr, r, arr.length - 1); // p2
+		
+		for(int val: arr){
+			System.out.print(val + " ");
+		}
+		System.out.println(".");
+		
+	}
+	//TODO
+
+}
